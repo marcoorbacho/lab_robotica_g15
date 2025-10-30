a3aa003bfae77fcf5e1cd142b006a8821efcb4ed
Marco Corbacho <marcocorbacho2004@gmail.com>
2025-10-30 13:07:22 +0100

    cambio windows.bat

--- docker/run_container_windows.bat
+++ docker/run_container_windows.bat
@@ -1,10 +1,12 @@
+docker rm lab_rob_container
+
 docker run -it ^
 --env="DISPLAY=host.docker.internal:0" ^
 --name lab_rob_container ^
 --net=host ^
 --privileged ^
---mount type=bind,source=C:\Users\[USER]\lab_rob_shared,target=/home/[USER]/lab_rob_shared ^
+--mount type=bind,source="C:\Users\Marco Corbacho\lab_rob_shared",target=/home/MarcoCorbacho/lab_rob_shared ^
 lab_rob_image ^
 bash
-    
+
 docker rm lab_rob_container
